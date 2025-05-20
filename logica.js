9724591bdd86c4cc413370675186d3888ac826d1
AdriCarrasco22 <adriancarrasco2307@gmail.com>
2025-05-20 21:39:25 +0200

    Update logica.js

--- logica.js
+++ logica.js
@@ -2,3 +2,8 @@ function calculaDistancia (angulo,vel-ini) {
   let distancia = val-ini * vel-ini * Math.sin(2*angulo)/9.81;
     return distancia;
 }
+
+function gradosAradianes(grados){
+  let radianes = grados*Math.PI/180;
+  return radianes;
+}
